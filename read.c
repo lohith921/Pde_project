53be98b09ee0c54fcbec24452a9ce489ed8c751b
lohith921 <nanuvala.lohith@gmail.com>
2016-03-17 15:30:57 -0500

    Trying to incorporate map of nodes, may be edges and pointers to parent and neighbours

--- read.c
+++ read.c
@@ -29,8 +29,9 @@ void print_tree(struct triangle *root){
 			return;
 		}
 	}
+	
 void insert(struct triangle *root,struct node p){
-		if((root->l == NULL) && (root->c == NULL) && (root->r == NULL)){
+		if(point_inside(root,p) && (root->l == NULL && root->c == NULL && root->r == NULL)){
 			struct triangle *t1,*t2,*t3;
 			t1 = (struct triangle*)malloc(sizeof(struct triangle));
 			t2 = (struct triangle*)malloc(sizeof(struct triangle));
@@ -48,40 +49,35 @@ void insert(struct triangle *root,struct node p){
 			t3->nodes[2] = p; t3->l = NULL; t3->c = NULL; t3->r = NULL;
 			root->r = t3;
 		}
+		else if( point_inside(root,p)){
+			insert(root->l, p);
+			insert(root->c, p);
+			insert(root->r, p);
+		}
+		else{
+			return;
+		}
 	}
 double dotp(struct node n1, struct node n2){
 	return (n1.x*n2.x + n1.y*n2.y);
 }	
 int point_inside(struct triangle *t, struct node n){
 	struct node v0, v1, v2;
-	double u,v,w,invDenom,dot00,dot01,dot02,dot11,dot12;
-	v0.x = t->nodes[3].x-t->nodes[0].x; v0.y = t->nodes[3].y-t->nodes[0].y;
-	v1.x = t->nodes[2].x-t->nodes[0].x; v1.y = t->nodes[2].y-t->nodes[0].y;
-	v2.x = n.x-t->nodes[0].x; v2.y = n.y-t->nodes[0].y;
-	dot00 = dotp(v0, v0);
-	dot01 = dotp(v0, v1);
-	dot02 = dotp(v0, v2);
-	dot11 = dotp(v1, v1);
-	dot12 = dotp(v1, v2);
-	printf("Dots are, 00=%f, 01=%f,02=%f,11=%f,12=%f\n",dot00,dot01,dot02,dot11,dot12);
-	invDenom = 1 / (dot00 * dot11 - dot01 * dot01);
-	u = (dot11 * dot02 - dot01 * dot12) * invDenom;
-	v = (dot00 * dot12 - dot01 * dot02) * invDenom;
-	w = 1-u-v;
-	printf("u = %f, v = %f, w = %f\n", u,v,w);
-	if( (u > 0) && (v > 0) && (w > 0))
-		return 1;
+	v0 = t->nodes[0]; v1 = t->nodes[1]; v2 = t->nodes[2];
+	double l1,l2,l3,denom;
+	denom = (v1.y-v2.y)*(v0.x-v2.x)+(v2.x-v1.x)*(v0.y-v2.y);
+	l1 = ((v1.y-v2.y)*(n.x-v2.x)+(v2.x-v1.x)*(n.y-v2.y))/denom;
+    l2 = ((v2.y-v0.y)*(n.x-v2.x)+(v0.x-v2.x)*(n.y-v2.y))/denom;
+    l3 = 1-l1-l2; 
+	//printf("l1 = %f, l2 = %f, l3 = %f\n", l1,l2,l3);
+	if( (0.0 < l1 && l1 < 1.0) && (l2 > 0.0 && l2 < 1.0) && (l3 > 0.0 && l3 < 1.0))
+		return 1; // point lies inside.
 	else
-		return 0;
+		return 0; // point lies outside.
 }
-// The following function is used to locate a tree node in which the input node lies
-struct triangle* locate(struct triangle *t, struct node n){
-	
-	
-	
-}
+
 int main(){
-    int nnodes,i;
+    int nnodes,i,*node_num, *t_num;
     double xavg=0, yavg=0;
     struct node *input_nodes; // to keep track of input nodes.
     struct triangle *T=NULL;  // The big triangle
@@ -92,11 +88,15 @@ int main(){
     fp = fopen("4.node","r");
     fscanf(fp, "%d %d %d %d\n",&nnodes,&dummy,&x,&y);
     input_nodes = (struct node* )malloc(sizeof(struct node)*nnodes);
+    map_t * node_map = map_create();
+    *node_num = 3; *t_num = 0;
     for(i = 0; i<nnodes; i++){
     	fscanf(fp,"%d %d %d\n",&dummy, &x, &y);
-    	printf("x = %d, y = %d\n",x,y);
-    	input_nodes[i].x = (double)x;
-    	input_nodes[i].y = (double)y;
+    	//printf("x = %d, y = %d\n",x,y);
+    	//input_nodes[i].x = (double)x;
+    	//input_nodes[i].y = (double)y;
+    	map_set(&node,node_num,x,y);
+    	node_num++;
     	if(x >= xmax) 
   			xmax = x;
     	else if(x <= xmin) 
@@ -108,23 +108,21 @@ int main(){
  	}
  	/*printf("xmax is %d, xmin is %d, ymax is %d, ymin is %d\n",xmax, xmin,ymax, ymin);	
     printf("left is %f , %f\n",(double)xmin-abs(xmax),(double)ymin-abs(ymax));
-    printf("right is %f, %f\n",(double)xmax+abs(xmin),(double)ymin+abs(ymax));
-    printf("top is %f , %f\n",(double)(abs(xmin)+xmax)/2,(double)ymax+abs(ymin));*/
+    printf("right is %f, %f\n",(double)xmax+abs(xmin),(double)ymin-abs(ymax));
+    printf("top is %f , %f\n",(double)(xmin+xmax)/2,(double)ymax+abs(ymin));*/
     // Initializing the big triangle.
     T = (struct triangle*)malloc(sizeof(struct triangle));
-    T->l = NULL; T->c = NULL; T->r = NULL;
+    T->l = NULL; T->c = NULL; T->r = NULL; T->tnum = ++t_num;
     T->nodes[0].x = (double)xmin-abs(xmax); T->nodes[0].y = (double)ymin-abs(ymax);
-    T->nodes[1].x = (double)xmax+abs(xmin); T->nodes[1].y = (double)ymin+abs(ymax);
-    T->nodes[2].x = (double)(abs(xmin)+xmax)/2; T->nodes[2].y = (double)ymax+abs(ymin);
-    //printf("Printing the original tree\n");
-    //print_tree(T);
-    //temp = (struct node)malloc(sizeof(struct node)*1);
-    //temp.x = -11.0; temp.y = -5.0;
-    temp.x = 0.0; temp.y = 0.0;
-    //insert(T,temp);
-    //printf("After inserting first point\n");
-    //print_tree(T);  
-    printf("checking for inside %d\n", point_inside(T,temp)); 
+    T->nodes[1].x = (double)xmax+abs(xmin); T->nodes[1].y = (double)ymin-abs(ymax);
+    T->nodes[2].x = (double)(xmin+xmax)/2; T->nodes[2].y = (double)ymax+abs(ymin);
+    temp.x = 0.0; temp.y = 10.0;
+    insert(T,temp);
+    print_tree(T);
+    temp.x = 0.0; temp.y = 7.0;
+    insert(T,temp);
+    print_tree(T);
+    //printf("checking for inside %d\n", point_inside(T,temp)); 
     return 0;
 }
     // *(*(nodes+i)+0) = (double)x;
@@ -138,3 +136,23 @@ int main(){
 // printf("# of nodes is %d %d %d %d\n",nnodes,dummy,x,y);
     //fscanf(fp, " %d %d %d\n",&dummy,&x,&y);
     //printf(" %d %d %d\n", dummy,x,y);
+	/*v0.x = t->nodes[3].x-t->nodes[0].x; v0.y = t->nodes[3].y-t->nodes[0].y;
+	v1.x = t->nodes[2].x-t->nodes[0].x; v1.y = t->nodes[2].y-t->nodes[0].y;
+	v2.x = n.x-t->nodes[0].x; v2.y = n.y-t->nodes[0].y;
+	dot00 = dotp(v0, v0);
+	dot01 = dotp(v0, v1);
+	dot02 = dotp(v0, v2);
+	dot11 = dotp(v1, v1);
+	dot12 = dotp(v1, v2);
+	printf("Dots are, 00=%f, 01=%f,02=%f,11=%f,12=%f\n",dot00,dot01,dot02,dot11,dot12);
+	invDenom = 1 / (dot00 * dot11 - dot01 * dot01);
+	u = (dot11 * dot02 - dot01 * dot12) * invDenom;
+	v = (dot00 * dot12 - dot01 * dot02) * invDenom;
+	w = 1-u-v;*/
+ //insert(T,temp);
+    //printf("After inserting first point\n");
+    //print_tree(T); 
+    //printf("Printing the original tree\n");
+    //print_tree(T);
+    //temp = (struct node)malloc(sizeof(struct node)*1);
+    //temp.x = -11.0; temp.y = -5.0;
