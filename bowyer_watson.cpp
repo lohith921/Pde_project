c64ab251d858be6569ff6acd9762a9099f44d67b
Lohith <nanuvala.lohith@gmail.com>
2016-03-27 10:29:30 -0500

    edited the file to remove spaces

--- bowyer_watson.cpp
+++ bowyer_watson.cpp
@@ -2,87 +2,83 @@
 #include<vector>
 
 struct Vertex{
-double  x,y,z;
-Vertex(double  X,double  Y,double Z):
-x(X),y(Y),z(Z){}
-};
+       double  x,y,z;
+       Vertex(double X,double Y,double Z):
+       x(X),y(Y),z(Z){}
+       };
 
-structEdge{
-Vertex*vmin,*vmax;
-Edge(Vertex*v1,Vertex*v2)
-vmin=std::min(v1,v2);
-vmax=std::max(v1,v2);
-}
-booloperator<(constEdge&other)const{
-if(vmin<other.vmin)returntrue;
-if(vmin>other.vmin)returnfalse;
-if(vmax<other.vmax)returntrue;
-returnfalse;
-}
-};
+struct Edge{
+       Vertex *vmin,*vmax;
+       Edge(Vertex *v1,Vertex *v2)
+       vmin = std::min(v1,v2);
+       vmax = std::max(v1,v2);
+       }      
+       bool operator <(const Edge &other)const{
+            if (vmin < other.vmin) return true;
+            if (vmin > other.vmin) return false;
+            if (vmax < other.vmax) return true;
+            return false;
+            }
+       };
 
-structFace{
-Face
-*F[3];
-Vertex*V[3];
-booldeleted;
-Face(Vertex*v0,Vertex*v1,Vertex*v2){
-V[0]=v0;V[1]=v1;V[2]=v2;
-F[0]=F[1]=F[2]=NULL;
-deleted=false;
-}
-EdgegetEdge(intk){
-returnEdge(V[k],V[(k+1)%3]);
-}
-boolinCircle(Vertex*c);
-Vertexcentroid();
-};
+struct Face{
+       Face *F[3];
+       Vertex*V[3];
+       bool deleted;
+       Face(Vertex *v0,Vertex *v1,Vertex *v2){
+                   V[0]=v0;V[1]=v1;V[2]=v2;
+                   F[0]=F[1]=F[2]=NULL;
+                   deleted = false;
+                   }
+       Edge getEdge(int k){
+            return Edge(V[k],V[(k+1)%3]);
+       }
+       bool inCircle(Vertex *c);
+       Vertex centroid();
+       };
 
-voidcomputeAdjacencies(std::vector<Face*>&cavity){
-std::map<Edge,std::pair<int,Face*>>edgeToFace;
-for(intiFace=0;iFace<cavity.size();iFace++){
-for(intiEdge=0;iEdge<3;iEdge++){
-Edgeedge=cavity[iFace]->getEdge(iEdge);
-std::map<Edge,std::pair<int,Face*>>::iteratorit=
-edgeToFace.find(edge);
-if(it==edgeToFace.end()){
-//edgehasnotyetbeentouched,socreateanentry
-edgeToFace.insert(std::make_pair(edge,
-std::make_pair(iEdge,cavity[iFace])));
-}
-else{
-//Connectthetwoneighboringtriangles
-cavity[iFace]->F[iEdge]=it->second.second;
-it->second.second->F[it->second.first]=cavity[iFace];
-//Eraseedgefromthemap
-edgeToFace.erase(it);
-}
-}
-}
+void computeAdjacencies(std::vector<Face*> &cavity){
+     std::map<Edge,std::pair<int,Face*>> edgeToFace;
+     for(int iFace = 0;iFace < cavity.size();iFace++){
+             for(int iEdge = 0;iEdge < 3;iEdge++){
+                     Edge edge = cavity[iFace]->getEdge(iEdge);
+                     std::map<Edge,std::pair<int,Face*>>::iteratorit = edgeToFace.find(edge);
+                     if (it == edgeToFace.end()){
+                               //edgehasnotyetbeentouched,socreateanentry
+                               edgeToFace.insert(std::make_pair(edge,std::make_pair(iEdge,cavity[iFace])));
+                     }
+                     else{
+                               //Connectthetwoneighboringtriangles
+                               cavity[iFace]->F[iEdge] = it->second.second;
+                               it->second.second->F[it->second.first] = cavity[iFace];
+                               //Eraseedgefromthemap
+                               edgeToFace.erase(it);
+                     }
+             }
+     }            
 }
 
-voiddelaunayCavity(Face*f,Vertex*v,std::vector<Face*>&cavity,
-std::vector<Edge>&bnd,std::vector<Face*>
-&otherSide){
-if(f->deleted)return;
-f->deleted=true;//Markthetriangle
-cavity.push_back(f);
-for(intiNeigh=0;iNeigh<3;iNeigh++){
-if(f->F[iNeigh]==NULL){
-bnd.push_back(f->getEdge(iNeigh));
-}
-elseif(!f->F[iNeigh]->inCircle(v)){
-bnd.push_back(f->getEdge(iNeigh));
-if(!f->F[iNeigh]->deleted){
-otherSide.push_back(f->F[iNeigh]);
-f->F[iNeigh]->deleted=true;
-}
-}
-elsedelaunayCavity(f->F[iNeigh],v,cavity,bnd,otherSide);
-}
+void delaunayCavity(Face *f,Vertex *v,std::vector<Face*> &cavity,std::vector<Edge> &bnd,std::vector<Face*>
+     &otherSide){
+     if (f->deleted) return;
+        f->deleted = true;//Markthetriangle
+        cavity.push_back(f);
+        for(int iNeigh = 0;iNeigh < 3;iNeigh++){
+                if (f->F[iNeigh] == NULL){
+                   bnd.push_back(f->getEdge(iNeigh));
+                }
+                else if(!f->F[iNeigh]->inCircle(v)){
+                   bnd.push_back(f->getEdge(iNeigh));
+                   if (!f->F[iNeigh]->deleted){
+                       otherSide.push_back(f->F[iNeigh]);
+                       f->F[iNeigh]->deleted = true;
+                   }
+                }
+                else delaunayCavity(f->F[iNeigh],v,cavity,bnd,otherSide);
+        }
 }
 
-Face*lineSearch(Face*f,Vertex*v){
+Face* lineSearch(Face *f,Vertex *v){
 while(1){
 if(f==NULL)returnNULL;//weshouldNEVERreturnhere
 if(f°>inCircle(v))returnf;
