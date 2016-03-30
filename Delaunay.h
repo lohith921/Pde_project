17a0001607a0a2f2f53718caeaa438ce4585110f
lohith921 <nanuvala.lohith@gmail.com>
2016-03-30 18:08:46 -0500

    Formatted to look good

--- Delaunay.h
+++ Delaunay.h
@@ -13,20 +13,20 @@ const int MaxTriangles = 1000;
 const int n_MaxPoints = 10; // for the test programm
 const double EPSILON = 0.000001;
 
-struct ITRIANGLE{
-  int p1, p2, p3;
+struct triangle{
+  int p1, p2, p3; // Node numbers
 };
 
-struct IEDGE{
+struct edge{
   int p1, p2;
 };
 
-struct XYZ{
+struct node{
   double x, y, z;
 };
 
-int XYZCompare(const void *v1, const void *v2);
-int Triangulate(int nv, XYZ pxyz[], ITRIANGLE v[], int &ntri);
+int nodeCompare(const void *v1, const void *v2);
+int Triangulate(int nv,node pxyz[], triangle v[], int &ntri);
 int CircumCircle(double, double, double, double, double, double, double, 
 double, double&, double&, double&);
 
